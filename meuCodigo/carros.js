4fb5f2afb92cc0280bd6c9a499ac63e2479508d3
Raylson Marques <mr.raylson11@gmail.com>
2022-06-21 21:17:19 -0300

    Update carros.js

--- meuCodigo/carros.js
+++ meuCodigo/carros.js
@@ -12,7 +12,7 @@ let xCarros = [600, 600, 600, 600, 600, 600];
 let yCarros = [40, 96, 150, 210, 270, 318];
 let comprimentoCarro = 50;
 let alturaCarro = 40;
-let velocidadeCarros = [1, 1.25, 1.7, 2.5, 1.65, 1.5];
+let velocidadeCarros = [1, 1.25, 1.7, 0.5, 1.65, 1.5];
 let colidiu = false;
 
 function criaCarro(img, x, y) {
